2ec566be3e3971c6d97cc8924dccaa4ff4d64cc5
Caleb Buahin <calebgh@gmail.com>
2015-01-28 12:59:59 -0700

    Branch for SWMM without global variables

---
+++ Source/SWMMOpenMINoGlobals/hash.h
@@ -0,0 +1,32 @@
+//-----------------------------------------------------------------------------
+//   hash.h
+//
+//   Header file for Hash Table module hash.c.
+//-----------------------------------------------------------------------------
+#ifndef HASH_H
+#define HASH_H
+
+//#include "globals.h"
+
+#define HTMAXSIZE 1999
+#define NOTFOUND  -1
+
+struct HTentry
+{
+    char   *key;
+    int    data;
+    struct HTentry *next;
+};
+typedef struct HTentry *HTtable;
+
+
+
+
+
+HTtable *HTcreate(void);
+int      HTinsert(HTtable *, char *, int);
+int      HTfind(HTtable *, char *);
+char    *HTfindKey(HTtable *, char *);
+void     HTfree(HTtable *);
+
+#endif
---
+++ Source/SWMMOpenMINoGlobals/swmmopenminoglobals.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "swmmopenminoglobals.h"
+
+SWMMOpenMINoGlobals::SWMMOpenMINoGlobals()
+{
+
+}
+
+SWMMOpenMINoGlobals::~SWMMOpenMINoGlobals()
+{
+
+}
